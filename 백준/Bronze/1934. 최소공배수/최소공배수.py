85b0123d48aef888b85611e14b13d9e88a3e941e
권하은 <103029075+Gwonhaeun0908@users.noreply.github.com>
2023-11-12 20:24:30 +0900

    [Bronze I] Title: 최소공배수, Time: 124 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for i in range(t) :
+    a, b = map(int , input().split())
+    result = a * b
+    
+    while b > 0 :
+        a, b = b, a % b
+        
+    print(result // a)
